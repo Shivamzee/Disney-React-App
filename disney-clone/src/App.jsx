d21d75e32e81b8eaf33d1b239ed02c28b1c0b58a
Shivamzee <hello.shivamzee@gmail.com>
2024-07-09 17:50:03 +0530

    Login-home page completed

--- disney-clone/src/App.jsx
+++ disney-clone/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./Components/Login";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route exact path="/" element={<Login />} />
         </Routes>
--- disney-clone/src/Components/Login.jsx
+++ disney-clone/src/Components/Login.jsx
@@ -5,6 +5,17 @@ const Login = () => {
   return (
     <Container>
       <Content>
+        <CTA>
+          <CTALogoOne src="/Images/cta-logo-one.svg" alt="CTA logo" />
+          <SignUp>Get It All There</SignUp>
+          <Description>
+            "Never stop fighting until you arrive at your destined place - the
+            unique you. Have an aim in life, continuously acquire knowledge,
+            work hard, and have perseverance to realise the great life." -Dr.
+            APJ Abdul Kalam || 25/07/2024
+          </Description>
+          <CTALogoTwo src="/Images/cta-logo-two.png" alt="Logo below desc" />
+        </CTA>
         <BgImage />
       </Content>
     </Container>
@@ -44,4 +55,50 @@ const BgImage = styled.div`
   left: 0;
   z-index: -1;
 `;
+
+const CTA = styled.div`
+  max-width: 650px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 650px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
+
+const CTALogoTwo = styled.img`
+  max-width: 650px;
+  margin-bottom: 20px;
+  vertical-align: bottom;
+  display: inline-block;
+  width: 100%;
+`;
+
+const SignUp = styled.a`
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  font-size: 18px;
+  padding: 16px 0;
+  border-radius: 4px;
+  border: 1px solid transparent;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
+
+const Description = styled.p`
+  line-height: 1.5;
+  margin: 0 0 24px;
+  letter-spacing: 1px;
+`;
+
 export default Login;
