0776276b3fcdbe012051f4c93ca061448d0d61df
Shivamzee <hello.shivamzee@gmail.com>
2024-07-09 11:42:15 +0530

    background login img randered

--- disney-clone/src/Components/Login.jsx
+++ disney-clone/src/Components/Login.jsx
@@ -1,8 +1,47 @@
 import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 
 const Login = () => {
-  return <div>Login Shivam</div>;
+  return (
+    <Container>
+      <Content>
+        <BgImage />
+      </Content>
+    </Container>
+  );
 };
 
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
+`;
+const Content = styled.div`
+  width: 100%;
+  padding: 80px 40px;
+  margin-bottom: 10vh;
+  height: 100%;
+  min-height: 100vh;
+  position: relative;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url("/Images/backgroundLogin.jpg");
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
 export default Login;
